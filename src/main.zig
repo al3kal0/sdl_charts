309498fdb8c0b4df25cff36f894d14f615bda034
raidenXR <70112686+raidenXR@users.noreply.github.com>
2022-07-20 08:44:26 -0400

    Add files via upload

--- src/main.zig
+++ src/main.zig
@@ -5,57 +5,61 @@ const SDL = @import("sdl2.zig");
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 
-    const flags = SDL.WindowFlags.FullscreenDesktop;
+    const flags = SDL.WindowFlags.Maximized;
     const window = SDL.CreateWindow("example", SDL.WindowPosCentered, SDL.WindowPosCentered, 800, 600, flags);
     defer SDL.DestroyWindow(window);
     const renderer = SDL.CreateRenderer(window, -1, SDL.RendererFlags.Accelarated);
     defer SDL.DestroyRenderer(renderer);
 
-    var vert: [3]c.SDL_Vertex = undefined;
+    // var vert: [3]c.SDL_Vertex = undefined;
+// 
+    // // center
+    // vert[0].position.x = 400;
+    // vert[0].position.y = 150;
+    // vert[0].color.r = 255;
+    // vert[0].color.g = 0;
+    // vert[0].color.b = 0;
+    // vert[0].color.a = 255;
+// 
+    // // left
+    // vert[1].position.x = 200;
+    // vert[1].position.y = 450;
+    // vert[1].color.r = 0;
+    // vert[1].color.g = 0;
+    // vert[1].color.b = 255;
+    // vert[1].color.a = 255;
+// 
+    // // right
+    // vert[2].position.x = 600;
+    // vert[2].position.y = 450;
+    // vert[2].color.r = 0;
+    // vert[2].color.g = 255;
+    // vert[2].color.b = 0;
+    // vert[2].color.a = 255;
 
-    // center
-    vert[0].position.x = 400;
-    vert[0].position.y = 150;
-    vert[0].color.r = 255;
-    vert[0].color.g = 0;
-    vert[0].color.b = 0;
-    vert[0].color.a = 255;
-
-    // left
-    vert[1].position.x = 200;
-    vert[1].position.y = 450;
-    vert[1].color.r = 0;
-    vert[1].color.g = 0;
-    vert[1].color.b = 255;
-    vert[1].color.a = 255;
-
-    // right 
-    vert[2].position.x = 600;
-    vert[2].position.y = 450;
-    vert[2].color.r = 0;
-    vert[2].color.g = 255;
-    vert[2].color.b = 0;
-    vert[2].color.a = 255;
-
-    var quit = SDL.False;
+    var quit = false;
 
     while(!quit)
     {
         var event: SDL.Event = undefined;
 
-        while(SDL.PollEvent(event))
+        while(SDL.PollEvent(&event))
         {
             switch(event.type)
             {
-                SDL.QUIT => quit = SDL.True,
-                else => unreachable,
+                c.SDL_QUIT => quit = true,
+                c.SDLK_ESCAPE => quit = true,
+                else => {},
             }
+
+            if(event.type == c.SDL_KEYDOWN and event.key.keysym.sym == c.SDLK_ESCAPE) quit = true;
         }
 
-        SDL.SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL.RenderClear(renderer);
+        // const color = SDL.Color{ .r = 125, .g =45, .b=89, .a = 255 };
+        try SDL.SetRenderDrawColor(renderer, .{ .r = 125, .g =45, .b=89, .a = 255 });
+        try SDL.RenderClear(renderer);
         
-        SDL.RenderGeometry(renderer, null, vert, null);
+        // SDL.RenderGeometry(renderer, null, vert, null);
         
         SDL.RenderPresent(renderer);
     }
--- src/sdl2.zig
+++ src/sdl2.zig
@@ -1,26 +1,15 @@
-
 const c = @import("c.zig");
-
-// pub const RenderGeometry = c.SDL_RenderGeometry;
-// pub const RenderDrawLines = c.SDL_RenderDrawLines;
-// pub const RenderDrawPoints = c.SDL_RenderDrawPoints;
-// pub const RenderDrawRects = c.SDL_RenderDrawRects;
-// pub const RenderFillRects = c.SDL_RenderFillRects;
-
-// pub const SetRenderDrawColor = c.SDL_SetRenderDrawColor;
-                               // c.SDL_RenderClear;
-                               // c.SDL_RenderPresent;
 
 // constants
 pub const Bool = c.SDL_bool;
 pub const False = c.SDL_FALSE;
 pub const True = c.SDL_TRUE;
-pub const QUIT = c.SDL_QUIT;
 
-pub const Event = ?*c.SDL_Event;
+
 pub const Window = ?*c.SDL_Window;
 pub const Renderer = ?*c.SDL_Renderer;
 pub const Texture = ?*c.SDL_Texture;
+pub const Event = c.SDL_Event;
 pub const Vertex = c.SDL_Vertex;
 pub const Point = c.SDL_Point;
 pub const Rect = c.SDL_Rect;
@@ -31,15 +20,15 @@ pub const WindowPosUndefined = c.SDL_WINDOWPOS_UNDEFINED;
 
 
 // window flags
-pub const WindowFullscreen = c.SDL_WINDOW_FULLSCREEN;
-pub const WindowFullscreenDesktop = c.SDL_WINDOW_FULLSCREEN_DESKTOP;
-pub const WindowShown = c.SDL_WINDOW_SHOWN;
-pub const WindowHidden = c.SDL_WINDOW_HIDDEN;
-pub const WindowBorderless = c.SDL_WINDOW_BORDERLESS;
-pub const WindowResizable = c.SDL_WINDOW_RESIZABLE;
-pub const WindowMinimized = c.SDL_WINDOW_MINIMIZED;
-pub const WindowMaximized = c.SDL_WINDOW_MAXIMIZED;
-pub const WindowAllowHighDPI = c.SDL_WINDOW_ALLOW_HIGHDPI;
+// pub const WindowFullscreen = c.SDL_WINDOW_FULLSCREEN;
+// pub const WindowFullscreenDesktop = c.SDL_WINDOW_FULLSCREEN_DESKTOP;
+// pub const WindowShown = c.SDL_WINDOW_SHOWN;
+// pub const WindowHidden = c.SDL_WINDOW_HIDDEN;
+// pub const WindowBorderless = c.SDL_WINDOW_BORDERLESS;
+// pub const WindowResizable = c.SDL_WINDOW_RESIZABLE;
+// pub const WindowMinimized = c.SDL_WINDOW_MINIMIZED;
+// pub const WindowMaximized = c.SDL_WINDOW_MAXIMIZED;
+// pub const WindowAllowHighDPI = c.SDL_WINDOW_ALLOW_HIGHDPI;
 
 /// An enumeration of window states.
 pub const WindowFlags = enum(u32)
@@ -66,10 +55,10 @@ pub const WindowFlags = enum(u32)
 
 
 // renderer flags
-pub const RendererSoftware = c.SDL_RENDERER_SOFTWARE;
-pub const RendererAccelarated = c.SDL_RENDERER_ACCELERATED;
-pub const RendererPresentVSync = c.SDL_RENDERER_PRESENTVSYNC;
-pub const RendererTargetTexture = c.SDL_RENDERER_TARGETTEXTURE;
+// pub const RendererSoftware = c.SDL_RENDERER_SOFTWARE;
+// pub const RendererAccelarated = c.SDL_RENDERER_ACCELERATED;
+// pub const RendererPresentVSync = c.SDL_RENDERER_PRESENTVSYNC;
+// pub const RendererTargetTexture = c.SDL_RENDERER_TARGETTEXTURE;
 
 
 /// An enumeration of flags used when creating a rendering context.
@@ -86,18 +75,13 @@ pub const RendererFlags = enum(u32)
 };
 
 /// Poll for currently pending events.
-pub inline fn PollEvent(event: Event) bool
+pub inline fn PollEvent(event: *Event) bool
 {
-    return if(c.SDL_PollEvent(event) != 0) true else false;
+    return if(c.SDL_PollEvent(event) == 1) true else false;
 }
 
 /// Create a window with the specified position, dimensions, and flags.
-pub inline fn CreateWindow(title: []const u8,
-                           x: u32,
-                           y: u32,
-                           w: u32,
-                           h: u32,
-                           flags: WindowFlags) Window
+pub inline fn CreateWindow(title: []const u8, x: u32, y: u32, w: u32, h: u32, flags: WindowFlags) Window
 {    
     return c.SDL_CreateWindow(@ptrCast([*c]const u8, &title),
                               @intCast(c_int, x),
@@ -129,7 +113,7 @@ pub inline fn MaximizeWindow(window: Window) void
 /// Set a window's fullscreen state.
 pub inline fn SetWindowFullscreen(window: Window, flags: WindowFlags) !void
 {
-    if(c.SDL_SetWindowFullscreen(window, @enumToInt(flags)) != 0) return Error;
+    if(c.SDL_SetWindowFullscreen(window, @enumToInt(flags)) != 0) return Error.SDL_error;
 }
 
 /// Restore the size and position of a minimized or maximized window.
@@ -200,18 +184,14 @@ pub const EXT = struct
 
 /// Render a list of triangles, optionally using a texture and indices into the vertex array Color 
 /// and alpha modulation is done per vertex (SDL_SetTextureColorMod and SDL_SetTextureAlphaMod are ignored).
-pub inline fn RenderGeometry(renderer: Renderer,
-                             texture: Texture,
-                             vertices: []Vertex,
-                             indices: ?[]u16
-                             ) !void
+pub inline fn RenderGeometry(renderer: Renderer, texture: Texture, vertices: []Vertex, indices: ?[]u16) !void
 {    
     if(c.SDL_RenderGeometry(renderer,
                             texture,
                             @ptrCast([*c]Vertex, vertices),
                             vertices.len,
                             @ptrCast([*c]c_int, indices),
-                            indices.len) != 0) return Error;
+                            indices.len) != 0) return Error.SDL_error;
 }
 
 /// Render a list of triangles, optionally using a texture and indices into the vertex arrays Color 
@@ -235,35 +215,31 @@ pub inline fn RenderGeometry(renderer: Renderer,
 /// Draw a series of connected lines on the current rendering target.
 pub inline fn RenderDrawLines(renderer: Renderer, points: []Point) !void
 {
-    if(c.SDL_RenderDrawLines(renderer,
-                             @ptrCast([*c]Point, points),
-                             points.len) != 0) return Error;
+    if(c.SDL_RenderDrawLines(renderer, @ptrCast([*c]Point, points), points.len) != 0) return Error.SDL_error;
 }
 
 /// Draw multiple points on the current rendering target.
 pub inline fn RenderDrawPoints(renderer: Renderer, points: []Point) !void
 {
-    if(c.SDL_RenderDrawPoints(renderer,
-                              @ptrCast([*c]Point, points),
-                              points.len) != 0) return Error;
+    if(c.SDL_RenderDrawPoints(renderer, @ptrCast([*c]Point, points), points.len) != 0) return Error.SDL_error;
 }
 
 /// Draw some number of rectangles on the current rendering target. 
 pub inline fn RenderDrawRects(renderer: Renderer, rects: []Rect) !void
 {
-    if(c.SDL_RenderDrawRects(renderer, @ptrCast([*c]Rect, rects), rects.len) != 0) return Error;
+    if(c.SDL_RenderDrawRects(renderer, @ptrCast([*c]Rect, rects), rects.len) != 0) return Error.SDL_error;
 }
 
 /// Fill some number of rectangles on the current rendering target with the drawing color.
 pub inline fn RenderFillRects(renderer: Renderer, rects: []Rect) !void
 {
-    if(c.SDL_RenderFillRects(renderer, @ptrCast([*c]Rect, rects), rects.len) != 0) return Error;
+    if(c.SDL_RenderFillRects(renderer, @ptrCast([*c]Rect, rects), rects.len) != 0) return Error.SDL_error;
 }
 
 /// Set the color used for drawing operations (Rect, Line and Clear).
 pub inline fn SetRenderDrawColor(renderer: Renderer, rgba: Color) !void
 {
-    if(c.SDL_SetRenderDrawColor(renderer, rgba.r, rgba.g, rgba.b, rgba.a) != 0) return Error;
+    if(c.SDL_SetRenderDrawColor(renderer, rgba.r, rgba.g, rgba.b, rgba.a) != 0) return Error.SDL_error;
 }
 
 /// Update the screen with any rendering performed since the previous call.
@@ -275,5 +251,5 @@ pub inline fn RenderPresent(renderer: Renderer) void
 /// Clear the current rendering target with the drawing color.
 pub inline fn RenderClear(renderer: Renderer) !void
 {
-    if(c.SDL_RenderClear(renderer) != 0) return Error;
+    if(c.SDL_RenderClear(renderer) != 0) return Error.SDL_error;
 }
